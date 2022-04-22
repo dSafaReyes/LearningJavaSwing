288ff5586050aa8e3d23103aaec688051acc2ab3
Jugador <dgavilanpedrozo@safareyes.es>
2022-04-22 20:51:06 +0200

    Commit 2 - Java 2D

--- src/main/java/ventanaPrincipal/LaminaPrincipal.java
+++ src/main/java/ventanaPrincipal/LaminaPrincipal.java
@@ -2,12 +2,17 @@ package ventanaPrincipal;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class LaminaPrincipal extends JPanel {
 
     public void paintComponent(Graphics g) {
+
         super.paintComponent(g);
-        g.drawRect(50, 50, 200, 200);
+        Graphics2D g2 = (Graphics2D) g;
+        Rectangle2D rectangle = new Rectangle2D.Double(100, 100, 200, 150);
+        g2.draw(rectangle);
+
     }
 
 }
